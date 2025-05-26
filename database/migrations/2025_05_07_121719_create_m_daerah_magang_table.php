a24edf321be453ae8abe63b4daf7c5ed17de9364
Khip01 <aakhifathallah24@gmail.com>
2025-05-26 19:10:25 +0700

    feat: change some migration db, model, etc
    changes and additions,
    migration:
    - added m_daerah_magang and m_provinsi migration table
    - removed m_lokasi_magang migration table
    model:
    - added DaerahMagangModel and ProvinsiModel
    - removed LokasiMagangModel
    seeder
    - added DaerahMagangSeeder and ProvinsiSeeder with dataset (in database/seeders/dataset/ directory)
    - removed LokasiMagangSeeder
    dataset:
    - added daerah and provinsi .csv dataset
    
    chore:
    - fixed some relation with other table like preferensi_mahasiswa_table and lowongan_magang_table

---
+++ database/migrations/2025_05_07_121719_create_m_daerah_magang_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_daerah_magang', function (Blueprint $table) {
+            $table->id('id_daerah_magang', 150);
+            $table->string('nama_daerah');
+            $table->enum('jenis_daerah', ['Kota', 'Kabupaten']);
+            $table->unsignedBigInteger('id_provinsi');
+            $table->foreign('id_provinsi')
+                  ->references('id_provinsi')
+                  ->on('m_provinsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_daerah_magang');
+    }
+};
