4ce5c57e14f029fe17dc9aa2bc9c675af3bfc957
Khip01 <aakhifathallah24@gmail.com>
2025-06-04 21:07:56 +0700

    feat:  configure cloudinary as cloud-based image storage
    +init cloudinary-laravel

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,13 @@ return [
             'throw' => false,
         ],
 
+        'cloudinary' => [
+            'driver' => 'cloudinary',
+            'api_key' => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'folder' => env('CLOUDINARY_FOLDER', 'bukti-magang'),
+        ],
     ],
 
     /*
