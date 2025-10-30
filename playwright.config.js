6066be4c36bbd218a891bf6734190fe63fc71c0b
TNCP06 <ctionusa6@gmail.com>
2025-10-30 23:16:11 +0700

    Menambahkan Playwright konfigurasi untuk browser testing

---
+++ playwright.config.js
@@ -0,0 +1,16 @@
+export default {
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+};
